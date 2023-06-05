084515711ca87a8858b57107a150cc674860995f
Bloogefest <bloogefest@gmail.com>
2023-06-05 21:17:32 +0300

    Изменение документации исключения `CollectionException`, документации и сигнатуры его некоторых конструкторов: `CollectionException(String)`, `CollectionException(Throwable)`, `CollectionException(String, Throwable)`, `CollectionException(boolean, boolean)`, `CollectionException(String, Throwable, boolean, boolean)` — и введение других: `CollectionException(String, boolean, boolean)`, `CollectionException(Throwable, boolean, boolean)` —, а также введение его поля `CollectionException.DEFAULT_MESSAGE`

--- src/main/java/com/bloogefest/collection/CollectionException.java
+++ src/main/java/com/bloogefest/collection/CollectionException.java
@@ -6,70 +6,126 @@
 
 package com.bloogefest.collection;
 
+import com.bloogefest.annotation.analysis.Contract;
 import com.bloogefest.annotation.analysis.NotNls;
+import com.bloogefest.annotation.analysis.Null;
 import com.bloogefest.annotation.analysis.Nullable;
 import com.bloogefest.common.SoftException;
 
 /**
- * Мягкое исключение коллекций, не требующее обязательного перехвата.
+ * Исключение коллекции.
  *
  * @since 1.0.0-RC1
  */
 public class CollectionException extends SoftException {
 
     /**
-     * Создаёт экземпляр по умолчанию.
+     * Сообщение по умолчанию.
      *
      * @since 1.0.0-RC1
      */
-    public CollectionException() {}
+    public static final @NotNls @Null String DEFAULT_MESSAGE = null;
 
     /**
-     * Создаёт экземпляр, используя переопределённое сообщение.
+     * Создаёт исключение по умолчанию.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("-> new")
+    public CollectionException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    /**
+     * Создаёт исключение на основе сообщения.
      *
      * @param message сообщение.
      *
      * @since 1.0.0-RC1
      */
-    public CollectionException(@NotNls @Nullable final String message) {
+    @Contract("_ -> new")
+    public CollectionException(final @NotNls @Nullable String message) {
         super(message);
     }
 
     /**
-     * Создаёт экземпляр, используя переопределённую причину.
+     * Создаёт исключение на основе причины.
      *
      * @param cause причина.
      *
      * @since 1.0.0-RC1
      */
-    public CollectionException(@Nullable final Throwable cause) {
+    @Contract("_ -> new")
+    public CollectionException(final @Nullable Throwable cause) {
         super(cause);
     }
 
     /**
-     * Создаёт экземпляр, используя переопределённое сообщение и причину.
+     * Создаёт исключение на основе сообщения и причины.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 1.0.0-RC1
      */
-    public CollectionException(@NotNls @Nullable final String message, @Nullable final Throwable cause) {
+    @Contract("_, _ -> new")
+    public CollectionException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * Создаёт экземпляр, используя переопределённое сообщение, причину, параметр подавления и записи трассировки стека
-     * по умолчанию.
+     * Создаёт исключение на основе параметров подавления и трассировки стека.
+     *
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _ -> new")
+    public CollectionException(final boolean suppression, final boolean writable) {
+        super(suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе сообщения, параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _, _ -> new")
+    public CollectionException(final String message, final boolean suppression, final boolean writable) {
+        super(message, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _, _ -> new")
+    public CollectionException(final Throwable cause, final boolean suppression, final boolean writable) {
+        super(cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе сообщения, причины, параметров подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
      * @param suppression параметр подавления.
-     * @param writable параметр записи трассировки стека.
+     * @param writable параметр трассировки стека.
      *
      * @since 1.0.0-RC1
      */
-    protected CollectionException(@NotNls @Nullable final String message, @Nullable final Throwable cause,
+    @Contract("_, _, _, _ -> new")
+    protected CollectionException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
                                   final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
     }
