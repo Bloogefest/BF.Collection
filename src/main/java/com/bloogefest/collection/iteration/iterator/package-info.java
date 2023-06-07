4d011418d8fdf0840e8e40de216c4d90d5a4ccfa
Bloogefest <bloogefest@gmail.com>
2023-06-07 23:33:11 +0300

    Введение пакета `com.bloogefest.collection.iteration.iterator`

---
+++ src/main/java/com/bloogefest/collection/iteration/iterator/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Содержит первичные инструменты, предназначенные для итерации по коллекциям.
+ *
+ * @since 1.0.0-RC1
+ */
+package com.bloogefest.collection.iteration.iterator;
