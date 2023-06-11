2a98daea9bc44506fb6bceb26198465cfd08d589
Bloogefest <bloogefest@gmail.com>
2023-06-11 17:21:17 +0300

    Введение исключения `IteratorAuxiliaryException`

---
+++ src/main/java/com/bloogefest/collection/iteration/iterator/IteratorAuxiliaryException.java
@@ -0,0 +1,135 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.collection.iteration.iterator;
+
+import com.bloogefest.annotation.analysis.Contract;
+import com.bloogefest.annotation.analysis.NotNls;
+import com.bloogefest.annotation.analysis.Null;
+import com.bloogefest.annotation.analysis.Nullable;
+
+/**
+ * Вспомогательное исключение итератора.
+ *
+ * @since 1.0.0-RC1
+ */
+public class IteratorAuxiliaryException extends IteratorException {
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 1.0.0-RC1
+     */
+    public static final @NotNls @Null String DEFAULT_MESSAGE = null;
+
+    /**
+     * Создаёт вспомогательное исключение итератора по умолчанию.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("-> new")
+    public IteratorAuxiliaryException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе сообщения.
+     *
+     * @param message сообщение.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_ -> new")
+    public IteratorAuxiliaryException(final @NotNls @Nullable String message) {
+        super(message);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе причины.
+     *
+     * @param cause причина.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_ -> new")
+    public IteratorAuxiliaryException(final @Nullable Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе сообщения и причины.
+     *
+     * @param message сообщение.
+     * @param cause причина.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _ -> new")
+    public IteratorAuxiliaryException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе параметров подавления и трассировки стека.
+     *
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _ -> new")
+    public IteratorAuxiliaryException(final boolean suppression, final boolean writable) {
+        super(suppression, writable);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе сообщения, параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _, _ -> new")
+    public IteratorAuxiliaryException(final @NotNls @Nullable String message, final boolean suppression,
+                                      final boolean writable) {
+        super(message, suppression, writable);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _, _ -> new")
+    public IteratorAuxiliaryException(final @Nullable Throwable cause, final boolean suppression,
+                                      final boolean writable) {
+        super(cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт вспомогательное исключение итератора на основе сообщения, причины, параметров подавления и трассировки
+     * стека.
+     *
+     * @param message сообщение.
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 1.0.0-RC1
+     */
+    @Contract("_, _, _, _ -> new")
+    protected IteratorAuxiliaryException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
+                                         final boolean suppression, final boolean writable) {
+        super(message, cause, suppression, writable);
+    }
+
+}
