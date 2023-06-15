01b4ed68328ddee85d9f3a2e9f0aac52a44c7d00
Bloogefest <bloogefest@gmail.com>
2023-06-15 17:58:18 +0300

    Изменение документации конструкторов: `IteratorElementCachingException()`, `IteratorElementCachingException(String)`, `IteratorElementCachingException(Throwable)`, `IteratorElementCachingException(String, Throwable)`, `IteratorElementCachingException(boolean, boolean)`, `IteratorElementCachingException(String, boolean, boolean)`, `IteratorElementCachingException(Throwable, boolean, boolean)` и `IteratorElementCachingException(String, Throwable, boolean, boolean)` —, а также тела: `IteratorElementCachingException()`, `IteratorElementCachingException(String)`, `IteratorElementCachingException(Throwable)`, `IteratorElementCachingException(String, Throwable)`, `IteratorElementCachingException(boolean, boolean)`, `IteratorElementCachingException(String, boolean, boolean)` и `IteratorElementCachingException(Throwable, boolean, boolean)`

--- src/main/java/com/bloogefest/collection/iteration/iterator/IteratorElementCachingException.java
+++ src/main/java/com/bloogefest/collection/iteration/iterator/IteratorElementCachingException.java
@@ -23,27 +23,24 @@ public class IteratorElementCachingException extends IteratorElementException {
      *
      * @since 1.0.0-RC1
      */
-    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to cache iterator element";
+    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to get iterator element";
 
     /**
-     * Шаблонное сообщение.
-     *
-     * @since 1.0.0-RC1
-     */
-    public static final @NotNls @NotNull String TEMPLATE_MESSAGE = "Failed to cache %s";
-
-    /**
-     * Создаёт исключение кэширования элемента итератора по умолчанию.
+     * Создаёт исключение кэширования элемента итератора на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @since 1.0.0-RC1
      */
     @Contract("-> new")
     public IteratorElementCachingException() {
-        super(DEFAULT_MESSAGE);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе сообщения.
+     * Создаёт исключение кэширования элемента итератора на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
@@ -51,11 +48,13 @@ public class IteratorElementCachingException extends IteratorElementException {
      */
     @Contract("_ -> new")
     public IteratorElementCachingException(final @NotNls @Nullable String message) {
-        super(message);
+        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе причины.
+     * Создаёт исключение кэширования элемента итератора на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию},
+     * переданной причины, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
@@ -63,11 +62,13 @@ public class IteratorElementCachingException extends IteratorElementException {
      */
     @Contract("_ -> new")
     public IteratorElementCachingException(final @Nullable Throwable cause) {
-        super(cause);
+        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе сообщения и причины.
+     * Создаёт исключение кэширования элемента итератора на основе переданного сообщения и причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -76,11 +77,12 @@ public class IteratorElementCachingException extends IteratorElementException {
      */
     @Contract("_, _ -> new")
     public IteratorElementCachingException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause);
+        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе параметров подавления и трассировки стека.
+     * Создаёт исключение кэширования элемента итератора на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
@@ -89,12 +91,12 @@ public class IteratorElementCachingException extends IteratorElementException {
      */
     @Contract("_, _ -> new")
     public IteratorElementCachingException(final boolean suppression, final boolean writable) {
-        super(suppression, writable);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе сообщения, параметров подавления и трассировки
-     * стека.
+     * Создаёт исключение кэширования элемента итератора на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param suppression параметр подавления.
@@ -105,11 +107,12 @@ public class IteratorElementCachingException extends IteratorElementException {
     @Contract("_, _, _ -> new")
     public IteratorElementCachingException(final @NotNls @Nullable String message, final boolean suppression,
                                            final boolean writable) {
-        super(message, suppression, writable);
+        this(message, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе причины, параметров подавления и трассировки стека.
+     * Создаёт исключение кэширования элемента итератора на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию},
+     * переданной причины, параметров подавления и трассировки стека.
      *
      * @param cause причина.
      * @param suppression параметр подавления.
@@ -120,11 +123,11 @@ public class IteratorElementCachingException extends IteratorElementException {
     @Contract("_, _, _ -> new")
     public IteratorElementCachingException(final @Nullable Throwable cause, final boolean suppression,
                                            final boolean writable) {
-        super(cause, suppression, writable);
+        this(DEFAULT_MESSAGE, cause, suppression, writable);
     }
 
     /**
-     * Создаёт исключение кэширования элемента итератора на основе сообщения, причины, параметров подавления и
+     * Создаёт исключение кэширования элемента итератора на основе переданного сообщения, причины, параметров подавления и
      * трассировки стека.
      *
      * @param message сообщение.
