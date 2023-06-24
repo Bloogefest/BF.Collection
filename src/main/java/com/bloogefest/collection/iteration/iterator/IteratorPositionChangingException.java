1e9aa9168711107a4ae8ae44a3607f0e3a055835
Bloogefest <bloogefest@gmail.com>
2023-06-25 00:41:57 +0300

    Изменение значения поля `IteratorPositionChangingException.DEFAULT_MESSAGE`

--- src/main/java/com/bloogefest/collection/iteration/iterator/IteratorPositionChangingException.java
+++ src/main/java/com/bloogefest/collection/iteration/iterator/IteratorPositionChangingException.java
@@ -23,7 +23,7 @@ public class IteratorPositionChangingException extends IteratorPositionException
      *
      * @since 1.0.0-RC1
      */
-    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to change iterator position";
+    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to change the iterator position";
 
     /**
      * Шаблонное сообщение.
